468d2383e113c23f8ac79a004a6aaebe133ada16
Danica3 <112400@emmauscollege.nl>
2021-12-20 11:05:21 +0000

    tabellen gemaakt

--- db/create.sql
+++ db/create.sql
@@ -23,6 +23,18 @@ CREATE TABLE collectie (
   name VARCHAR(255)
 );
 
+DROP TABLE IF EXISTS kleuren; 
+CREATE TABLE kleuren (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR(255)
+);
+
+DROP TABLE IF EXISTS kleur_kleding; 
+CREATE TABLE collectie (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  products_id INTEGER,
+  kleuren_id INTEGER
+);
 --
 -- populate with data
 --
@@ -32,16 +44,31 @@ CREATE TABLE collectie (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('Ravenclaw beanie', 'Een warme wollen muts in de kleuren van Ravenclaw.', '806542899-3', 10.50);
-insert into products (name, description, code, price) values ('Hufflepuff beanie', 'Een warme wollen muts in de kleuren van Hufflepuff.', '816905633-0', 10.50);
-insert into products (name, description, code, price) values ('Slytherin beanie', 'Een warme wollen muts in de kleuren van Slytherin.', '077030122-3', 10,50);
-insert into products (name, description, code, price) values ('Gryffindor beanie', 'Een warme wollen muts in de kleuren van Gryffindor.', '445924201-X', 10.50);
-insert into products (name, description, code, price) values ('Ravenclaw sweater', 'Een gezellige gebreide trui met een Ravenclaw thema, waardoor het net voelt alsof je echt op Hogwarts zit.', '693155505-7', 30);
-insert into products (name, description, code, price) values ('Hufflepuff sweater', 'Een gezellige gebreide trui met een Hufflepuff thema, waardoor het net voelt alsof je echt op Hogwarts zit.', '686928463-6', 30);
-insert into products (name, description, code, price) values ('Slytherin sweater', 'Een gezellige gebreide trui met een Slytherin thema, waardoor het net voelt alsof je echt op Hogwarts zit.', '492662523-7', 30);
-insert into products (name, description, code, price) values ('Gryffindor sweater','Een gezellige gebreide trui met een Gryffindor thema, waardoor het net voelt alsof je echt op Hogwarts zit.', '629572923-7', 30);
-insert into products (name, description, code, price) values ('Ravenclaw scarf', 'Een dikke sjaal om je warm te houden tijdens koude dagen met het logo van Ravenclaw erop.', '946217899-7', 12.50);
-insert into products (name, description, code, price) values ('Hufflepuff scarf', 'Een dikke sjaal om je warm te houden tijdens koude dagen met het logo van Hufflepuff erop.', '875470942-6', 12.50);
-insert into products (name, description, code, price) values ('Slytherin scarf', 'Een dikke sjaal om je warm te houden tijdens koude dagen met het logo van Slytherin erop.', '274396510-4', 12.50);
-insert into products (name, description, code, price) values ('Gryffindor scarf', 'Een dikke sjaal om je warm te houden tijdens koude dagen met het logo van Gryffindor erop.', '930321697-9', 12.50);
+insert into products (name, description, code, price, category_id, collectie_id, kleur_kleding_id) values ('Ravenclaw beanie', 'Een warme wollen muts in de kleuren van Ravenclaw.', '806542899-3', 10.50);
+insert into products (name, description, code, price, category_id, collectie_id, kleur_kleding_id) values ('Hufflepuff beanie', 'Een warme wollen muts in de kleuren van Hufflepuff.', '816905633-0', 10.50);
+insert into products (name, description, code, price, category_id, collectie_id, kleur_kleding_id) values ('Slytherin beanie', 'Een warme wollen muts in de kleuren van Slytherin.', '077030122-3', 10.50);
+insert into products (name, description, code, price, category_id, collectie_id, kleur_kleding_id) values ('Gryffindor beanie', 'Een warme wollen muts in de kleuren van Gryffindor.', '445924201-X', 10.50);
+insert into products (name, description, code, price, category_id, collectie_id, kleur_kleding_id) values ('Ravenclaw sweater', 'Een gezellige gebreide trui met een Ravenclaw thema, waardoor het net voelt alsof je echt op Hogwarts zit.', '693155505-7', 30);
+insert into products (name, description, code, price, category_id, collectie_id, kleur_kleding_id) values ('Hufflepuff sweater', 'Een gezellige gebreide trui met een Hufflepuff thema, waardoor het net voelt alsof je echt op Hogwarts zit.', '686928463-6', 30);
+insert into products (name, description, code, price, category_id, collectie_id, kleur_kleding_id) values ('Slytherin sweater', 'Een gezellige gebreide trui met een Slytherin thema, waardoor het net voelt alsof je echt op Hogwarts zit.', '492662523-7', 30);
+insert into products (name, description, code, price, category_id, collectie_id, kleur_kleding_id) values ('Gryffindor sweater','Een gezellige gebreide trui met een Gryffindor thema, waardoor het net voelt alsof je echt op Hogwarts zit.', '629572923-7', 30);
+insert into products (name, description, code, price, category_id, collectie_id, kleur_kleding_id) values ('Ravenclaw scarf', 'Een dikke sjaal om je warm te houden tijdens koude dagen met het logo van Ravenclaw erop.', '946217899-7', 12.50);
+insert into products (name, description, code, price, category_id, collectie_id, kleur_kleding_id) values ('Hufflepuff scarf', 'Een dikke sjaal om je warm te houden tijdens koude dagen met het logo van Hufflepuff erop.', '875470942-6', 12.50);
+insert into products (name, description, code, price, category_id, collectie_id, kleur_kleding_id) values ('Slytherin scarf', 'Een dikke sjaal om je warm te houden tijdens koude dagen met het logo van Slytherin erop.', '274396510-4', 12.50);
+insert into products (name, description, code, price, category_id, collectie_id, kleur_kleding_id) values ('Gryffindor scarf', 'Een dikke sjaal om je warm te houden tijdens koude dagen met het logo van Gryffindor erop.', '930321697-9', 12.50);
 
+-- category tabel
+insert into category (name) values ('beanie');
+insert into category (name) values ('sweater');
+insert into category (name) values ('scarf');
+
+-- collectie tabel
+insert into collectie (name) values ('Ravenclaw');
+insert into collectie (name) values ('Hufflepuff');
+insert into collectie (name) values ('Slytherin');
+insert into collectie (name) values ('Gryffindor');
+
+-- kleuren tabel
+insert into kleuren (name) values ('rood');
+insert into kleuren (name) values ('groen');
+insert into kleuren (name) values ('blauw');
